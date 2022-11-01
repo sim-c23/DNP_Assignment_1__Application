412b75499682640b78ee4a9ee69908c2bf805379
sajit <sajithijan1245@gmail.com>
2022-11-01 14:20:15 +0100

    Changes

---
+++ Web Application/BlazorWASM/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWASM;
+using HttpClient.ClientInterfaces;
+using HttpClient.Implementations;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new System.Net.Http.HttpClient { BaseAddress = new Uri("https://localhost:7093") });
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+await builder.Build().RunAsync();
